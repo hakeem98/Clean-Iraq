6e397bd464515d2bdbd68fb228b485a56684b36d
hiba <hiba.waad1@gmail.com>
2023-08-13 17:05:11 +0300

    edit the contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -6,15 +6,15 @@ const Contact = () => {
     <div className='p-10 max-w-3xl mx-auto mt-10 w-auto text-center'>
       <h1 className='text-3xl font-bold font-sans underline decoration-green-300 decoration-8  underline-offset-8 '>تواصل معنا</h1>
         <form className='  py-4 mt-4 border-t flex flex-col gap-5  w-auto text-right '>
-          <div className='flex flex-col gap-2'>
+          <div className='flex flex-col gap-1'>
             <label htmlFor='fullname '> الاسم</label>
             <input className=' text-right  px-3 py-2   border border-solid bg-red-50 rounded-md' type='text' id='fullname'></input>
           </div>
-          <div className='flex flex-col gap-2'>
+          <div className='flex flex-col gap-1'>
             <label htmlFor='email'>البريد الالكتروني</label>
             <input className=' text-right  px-3 py-2  border border-solid bg-red-50 rounded-md' type='text' id='email' ></input>
           </div>
-           <div className='flex flex-col gap-2'>
+           <div className='flex flex-col gap-1'>
             <label htmlFor='message'>ماهي رسالتك</label>
             <textarea className=' text-right h-36  px-3 py-2   border border-solid bg-red-50 rounded-md' id='message' ></textarea>
            </div>
